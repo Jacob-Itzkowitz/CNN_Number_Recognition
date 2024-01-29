7ad2ac84cae254b9eef2d28925ce333267030bfc
jacob.itzkowitz <jacobm.itzkowitz@gmail.com>
2024-01-29 17:37:12 -0500

    reorginized images and did edge detection

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-Work Process
+Work Process:
+
+Initially I did edge detection to find the numbers and put it into a Machine learning model to detect the numbers
+Next I created  a matrix of RGB values and just putting that through a Machine learning model
+
 Data collection:
   I started by drawing numbers on a white piece of paper then taking pictures and downloading them on my computer in the Pycharms directory
-  
+
--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-#Data
+from PIL import Image
+import numpy as np
+import cv2
+
+#opens image and saves it to a variable
+Zero = cv2.imread(r"C:\Users\jacob.itzkowitz\PycharmProjects\ImageRecognitionNumbers\Images\0.jpg")
+
+#gray scale image to reduce noise
+grayscale_image = cv2.cvtColor(Zero, cv2.COLOR_BGR2GRAY)
+
+#finding edges
+edges = cv2.Canny(grayscale_image, 50, 150, apertureSize=3)
+
+Lines_list = []
+lines = cv2.HoughLinesP(edges, 1, np.pi/180, 15, minLineLength=5, maxLineGap=10)
+
+for points in lines:
+    x, y, x1, y1 = points[0]
+    cv2.line(Zero, (x, y), (x1, y1), (0, 255, 0), 2)
+    Lines_list.append([(x, y), (x1, y1)])
+
+
+cv2.imwrite("Lines.png", Zero)
+
+
 
